c396c80fea6f9955405e035aa240a7aa761d7ab8
Kenno104 <kennec31@tcd.ie>
2023-03-03 12:44:07 +0000

    quick fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-url: 'https://forms.gle/uSaFmv8ZNv33yyd78'
+# url: 'https://forms.gle/uSaFmv8ZNv33yyd78'
 
 #Page Configuration
 st.set_page_config(page_title='Acclimatise Demo',
